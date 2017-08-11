bf0de78ad78fe31e06c9196800fb3b518c9e9014
Zsofia <zsofia.helmeczi@gmail.com>
2017-08-11 11:12:09 +0200

    Make a Constructor Challenge

--- 1_Dice/4_dice_returning_values_finished/dice.js
+++ 1_Dice/4_dice_returning_values_finished/dice.js
@@ -1,7 +1,17 @@
-var dice = {
-  sides: 6,
-  roll: function () {
+// var dice = {
+//   sides: 6,
+//   roll: function () {
+//     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//     return randomNumber;
+//   }
+// }
+
+function Dice(sides) {
+  this.sides = sides;
+  this.roll = function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
   }
 }
+
+var dice = new Dice(6);
