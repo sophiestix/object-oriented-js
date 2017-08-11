953b36b5dc166e7f31d7d1b9195849877f32e2ac
Zsofia <zsofia.helmeczi@gmail.com>
2017-08-11 15:46:22 +0200

    Updating the Song Object

--- 3_Treetunes/4_treetunes_updating_song_start/song.js
+++ 3_Treetunes/4_treetunes_updating_song_start/song.js
@@ -1,17 +1,20 @@
+// function Song(title, artist, duration) {
+//   var song = this; //The song variable is a new instance of the song when we call the song constructor,
+//   Media.call(song, title, duration); //Remember the media constructor takes two parameters, title and duration.
+//   //So the first parameter here is what we're telling the media function call to use as this.
+//   //this call is decorating the song instance with new properties - title, duration
+//   this.artist = artist;
+// }
+
 function Song(title, artist, duration) {
-  this.title = title;
+  Media.call(this, title, duration);
   this.artist = artist;
-  this.duration = duration;
-  this.isPlaying = false;
 }
 
-Song.prototype.play = function() {
-  this.isPlaying = true;
-};
-
-Song.prototype.stop = function() {
-  this.isPlaying = false;
-};
+Song.prototype = Object.create(Media.prototype); 
+//What this basically does is that it copies the references to the Media's
+//prototype properties and methods to the song's prototype.
+//We're creating a prototype chain.
 
 Song.prototype.toHTML = function() {
   var htmlString = '<li';
