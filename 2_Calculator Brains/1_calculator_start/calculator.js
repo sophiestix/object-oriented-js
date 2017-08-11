10faf989a387987a46f3452993372e22d884fff3
Zsofia <zsofia.helmeczi@gmail.com>
2017-08-11 10:39:36 +0200

    Modifying Objects with Methods

--- 2_Calculator Brains/1_calculator_start/calculator.js
+++ 2_Calculator Brains/1_calculator_start/calculator.js
@@ -1,12 +1,13 @@
 var calculator = {
 		sum: 0,
 		add: function(value) {
-
+      // this.sum = this.sum + value;
+      this.sum += value;
     },
     clear: function() {
-
+      this.sum = 0;
     }, 
     equals: function() {
-
+      return this.sum;
     }
 }
