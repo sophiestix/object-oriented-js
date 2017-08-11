20693cce5e55b8dec4b929e90b2340a9b3d54dad
Zsofia <zsofia.helmeczi@gmail.com>
2017-08-11 11:23:38 +0200

    Methods with Protoypes

--- 1_Dice/4_dice_returning_values_finished/dice.js
+++ 1_Dice/4_dice_returning_values_finished/dice.js
@@ -6,12 +6,17 @@
 //   }
 // }
 
+
 function Dice(sides) {
   this.sides = sides;
-  this.roll = function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
+}
+
+Dice.prototype.roll = function diceRoll () {
+  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  return randomNumber;
 }
 
 var dice = new Dice(6);
+var dice10 = new Dice(10);
+
+console.log(dice.roll === dice10.roll);
